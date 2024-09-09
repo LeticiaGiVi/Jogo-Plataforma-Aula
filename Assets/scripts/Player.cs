e72d0170542baae4c8fb2d27097118f963baa89f
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-09 12:57:06 -0300

    animação

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -7,11 +7,13 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     private float horizontal;
     private bool isFacingRight = true;
+    private Animator animator;
 
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();    
     }
     void Update()
     {
@@ -19,6 +21,8 @@ public class Player : MonoBehaviour
         Debug.Log(horizontal);
 
         this.rb.velocity = new Vector2(horizontal *8f, this.rb.velocity.y);
+
+        animator.SetFloat("speed",Mathf.Abs(horizontal));
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
